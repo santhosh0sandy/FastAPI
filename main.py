ce6b042953b673f6d16d505182aa812271713ad4
Santhoshkumar Murugan <santhoshkrish681@gmail.com>
2025-02-05 11:27:57 +0530

    push

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from pydantic import BaseModel
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
 
 fake_db = {}
 
